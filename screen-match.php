481c8b0acc608d521429e1c56b666bdd3f6ec590
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-24 20:59:48 -0300

    iniciando o screen match

---
+++ screen-match.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Bem-vindo(a) ao screen match!
+";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo $notaFilme;
