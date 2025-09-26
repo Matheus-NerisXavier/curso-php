802a86aeaf7055f2931bcf4a3d875a9b3d82d8f0
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-25 23:10:38 -0300

    exercicio contagem regressiva

---
+++ exercicios/25-09-2025/contagem_regressiva.php
@@ -0,0 +1,12 @@
+<!-- Escreva um programa que inicie com um número (ex: 10) e faça uma contagem regressiva até 0. A cada segundo (opcional, um desafio extra!), exiba o número na tela, e no final, exiba a mensagem "FOGO!". -->
+<?php
+
+$numero = 10;
+        
+while ($numero >= 0) {
+  echo "$numero\n";
+  sleep(1);
+  $numero --;
+}
+  echo "Fogo";
+?>
