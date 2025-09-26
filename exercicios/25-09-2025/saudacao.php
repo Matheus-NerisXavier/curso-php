b941476839de760ddfa94df48c05866754f5c0db
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-25 22:35:26 -0300

    exercicio de saudação

---
+++ exercicios/25-09-2025/saudacao.php
@@ -0,0 +1,14 @@
+<!-- Desenvolva um programa que exiba na tela uma saudação (bom dia, boa tarde ou boa noite) dependendo do horário encontrado em uma variável (inteiro representando as horas).
+ -->
+
+<?php 
+$horario = 18;
+
+if ($horario < 12) {
+  echo "Bom dia!";
+} else if ($horario < 18) {
+  echo "Boa tarde!";
+} else {
+  echo "Boa noite!";
+}
+?>
