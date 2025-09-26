29d9bedbafa7b914f93e2d47ad4e63f5527e7b4d
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-26 15:37:16 -0300

    exercicio nomes familiares

---
+++ exercicios/26-09-2025/nomes_familiares.php
@@ -0,0 +1,17 @@
+<!-- Crie um array contendo nomes de familiares seus e após sua criação adicione mais elementos ao array.
+ -->
+
+<?php
+$nomesParentes = [
+  'Lucia',
+  'Lucas',
+  'Jose',
+  'Isabela',
+  'Bruna'
+];
+
+array_push($nomesParentes, 'Pedro', 'Guilherme');
+
+var_dump($nomesParentes);
+
+?>
