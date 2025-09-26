7d1bb620e9668636444f61a806e5e99c4cd1b006
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-25 22:17:28 -0300

    exercicio numeros impares

---
+++ exercicios/25-09-2025/numeros_impares.php
@@ -0,0 +1,10 @@
+<!-- Escreva um programa que exiba, na tela do usuário, todos os números ímpares de 0 à 100.
+ -->
+
+<?php
+for ($i=0; $i <= 100; $i++) {
+  if ($i % 2 != 0) {
+    echo "$i\n";
+  }
+}
+?>
