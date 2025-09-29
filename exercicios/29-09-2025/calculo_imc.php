e113aa7adcf91293ace4cf198a5077f2b271b4d6
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-29 07:40:37 -0300

    calculo imc com funcao

---
+++ exercicios/29-09-2025/calculo_imc.php
@@ -0,0 +1,25 @@
+<!-- Crie uma função em PHP que calcule o IMC baseado na altura e peso passados por parâmetro -->
+
+<?php 
+function calcularImc (float $peso, float $altura): string {
+  $calculoImc = $peso / ($altura * $altura);
+  $imcFormatado = number_format($calculoImc, 2, ',', '.');
+
+  if ($calculoImc < 18.5) {
+    $classificacao = "Magreza";
+  } else if ($calculoImc < 24.9) {
+    $classificacao = "Normal";
+  } else if ($calculoImc < 29.9) {
+    $classificacao = "Sobrepeso";
+  } else {
+    $classificacao = "Obesidade";
+  }
+
+  return "Seu IMC é $imcFormatado e sua classifição foi: $classificacao";
+  
+}
+
+$diagnostico = calcularImc(80, 1.70);
+echo $diagnostico;
+
+?>
