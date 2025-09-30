d7215ac15ea96fcb9b3f4b614fe16ab0aa7e1b71
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-30 17:55:48 -0300

    primeiro exercicio poo

---
+++ exercicios/30-08-2025/ex_poo.php
@@ -0,0 +1,94 @@
+<!-- Praticar é muito importante! Por isso, preparamos um exercício para você colocar em prática o conteúdo abordado nesta aula.
+
+Levando em consideração o código a seguir: -->
+
+<?php
+
+class Conta
+{
+    private int $saldoEmCentavos;
+    private string $nomeTitular;
+    private string $numeroConta;
+
+    public function setSaldoEmCentavos(int $saldoEmCentavos): void
+    {
+        $this->saldoEmCentavos = $saldoEmCentavos;
+    }
+
+    public function getSaldoEmCentavos(): int
+    {
+        return $this->saldoEmCentavos;
+    }
+
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNumeroConta(int $numeroConta): void
+    {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getNumeroConta(): string
+    {
+        return $this->numeroConta;
+    }
+}
+
+?>
+// Como podemos torná-lo um pouco mais robusto, impedindo que o saldo seja manipulado livremente, por exemplo? Altere o código da classe para que o saldo seja alterado somente através das operações de saque e depósito.
+
+<?php
+
+class Conta
+{
+    private int $saldoEmCentavos;
+    private string $nomeTitular;
+    private string $numeroConta;
+
+    public function depositar($valorADepositar): void 
+    {
+      if($valorADepositar > 0) {
+        $this->saldoEmCentavos += $valorADepositar;
+      }
+    }
+
+    public function sacar($valorASacar): void
+    {
+      if($valorASacar > 0 && $valorASacar <= $this->saldoEmCentavos) {
+        $this->saldoEmCentavos -= $valorASacar;
+      }
+    }
+
+    public function getSaldoEmCentavos(): int
+    {
+        return $this->saldoEmCentavos;
+    }
+
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNumeroConta(int $numeroConta): void
+    {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getNumeroConta(): string
+    {
+        return $this->numeroConta;
+    }
+}
+?>
