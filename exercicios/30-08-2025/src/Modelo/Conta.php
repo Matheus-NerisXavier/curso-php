a0a8b5f78454e8c89eded5684f9af50093d1e1b1
Matheus Neris <matheus.nerisxa@outlook.com>
2025-09-30 22:15:32 -0300

    exercicio poo Conta

---
+++ exercicios/30-08-2025/src/Modelo/Conta.php
@@ -0,0 +1,30 @@
+<!-- Crie uma classe que represente uma conta com as propriedades saldo, nome do titular e tipo. Use os tipos e formas de acesso adequadas. -->
+
+<?php
+require '/src/Modelo/TipoConta.php';
+
+class Conta {
+  private int $saldoEmCentavos;
+
+  public function __construct(
+    public readonly string $nomeTitular,
+    public readonly TipoConta $tipo, 
+  )
+  {
+    $this->saldoEmCentavos = 0;
+  }
+
+  public function depositar(int $valorADepositar): void 
+  {
+    if($valorADepositar > 0) {
+      $this->saldoEmCentavos += $valorADepositar;
+    }
+  }
+
+  public function sacar(int $valorASacar): void
+  {
+    if($valorASacar > 0 && $valorASacar <= $this->saldoEmCentavos) {
+     $this->saldoEmCentavos -= $valorASacar;
+    }
+  }
+}
