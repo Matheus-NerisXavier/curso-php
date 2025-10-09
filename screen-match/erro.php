cac5059d8ff29f4d3c90f3bd8c00a221a530d74b
Matheus Neris <matheus.nerisxa@outlook.com>
2025-10-09 07:21:21 -0300

    Tratamento de exceções

---
+++ screen-match/erro.php
@@ -0,0 +1,15 @@
+<?php
+
+use ScreenMath\Calculos\ConversorNotaEstrela;
+use ScreenMath\Modelo\Episodio;
+use ScreenMath\Modelo\Genero;
+use ScreenMath\Modelo\Serie;
+
+require 'autoload.php';
+
+$serie = new Serie('Nome da serie', 2024, Genero::Acao, 7, 20,30);
+$episodio = new Episodio($serie, 'Piloto', 1);
+$episodio->avalia(10);
+
+$conversor = new ConversorNotaEstrela();
+echo $conversor->converte($episodio);
